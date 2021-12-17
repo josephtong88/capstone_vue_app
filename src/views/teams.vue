0c056d7c23325fa11519b327da31e76d914ef7b0
“Joseph <joseph@josephs-air.lan>
2021-12-16 19:18:34 -0800

    hard coded second parameter of opposing team. need to update backend

--- src/views/teams.vue
+++ src/views/teams.vue
@@ -4,6 +4,43 @@
 <p> Select date parameters if required between:</p>
     <p><input type="text" v-model="start_date"> start date </p>
     <p><input type="text" v-model="end_date"> end date </p>
+
+    <select v-model="opp">
+                    <option disabled value="">Please select one</option>
+                    <option>Washington Football Team</option>
+                    <option>Baltimore Ravens</option>
+                    <option>Green Bay Packers</option>
+                    <option>San Francisco 49ers</option>
+                    <option>Denver Broncos</option>
+                    <option>Cincinnati Bengals</option>
+                    <option>Houston Texans</option>
+                    <option>Indianapolis Colts</option>
+                    <option>Jacksonville Jaguars</option>
+                    <option>Miami Dolphins</option>
+                    <option>New England Patriots</option>
+                    <option>New York Giants</option>
+                    <option>New Orleans Saints</option>
+                    <option>Dallas Cowboys</option>
+                    <option>Detroit Lions</option>
+                    <option>Tampa Bay Buccaneers</option>
+                    <option>Los Angeles Chargers</option>
+                    <option>Kansas City Chiefs</option>
+                    <option>Seattle Seahawks</option>
+                    <option>Las Vegas Raiders</option>
+                    <option>Buffalo Bills</option>
+                    <option>Carolina Panthers</option>
+                    <option>Chicago Bears</option>
+                    <option>Cleveland Browns</option>
+                    <option>Minnesota Vikings</option>
+                    <option>New York Jets</option>
+                    <option>Philadelphia Eagles</option>
+                    <option>Tennessee Titans</option>
+                    <option>Atlanta Falcons</option>
+                    <option>Arizona Cardinals</option>
+                    <option>Pittsburgh Steelers</option>
+                    <option>Los Angeles Rams</option>
+                  </select>
+
     <button v-on:click="datedindexscores" > Submit </button>
     <div v-for="score in scores" v-bind:key="score.id">
       <p>Date: {{ score.date }}</p>
@@ -30,7 +67,8 @@ export default {
       message: "Hello from Script (JS)",
       team: {},
       start_date: "",
-      end_date: ""
+      end_date: "",
+      opp:""
     };
   },
   created: function () {
